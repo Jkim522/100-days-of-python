831ef8272384e53e788507afe014ce73c4680c69
Jkim522 <Jinseok.Kim522@gmail.com>
2023-09-12 16:33:39 -0400

    Update band_name_generator.py

--- day01/band_name_generator.py
+++ day01/band_name_generator.py
@@ -1,1 +1,11 @@
+#Welcome statement
+print("Welcome to the Band Name Generator")
 
+#Ask for input of the city name
+city = input("What's the name of the city you grew up in?\n")
+
+#Ask for input of pet name
+pet = input("What's your pet's name?\n")
+
+#Print the band name
+print(f"Your band name could be {city} {pet}")
