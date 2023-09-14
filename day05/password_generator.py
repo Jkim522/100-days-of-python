55ecb7a7fe6eddfe507dd973041bbda97276e3fb
Jkim522 <Jinseok.Kim522@gmail.com>
2023-09-14 16:45:52 -0400

    Update password_generator.py

--- day05/password_generator.py
+++ day05/password_generator.py
@@ -1,1 +1,30 @@
+import random
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'A', 'B', 'C', 'D', 'E',
+           'F', 'G', 'H', 'I', 'J', 'K']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to PyPassword Generator")
+nr_letters = int(input("How many letters would you like? \n"))
+nr_numbers = int(input("How many numbers would you like? \n"))
+nr_symbols = int(input("How many symbols would you like? \n"))
+
+password_list = []
+
+for x in range(nr_letters):
+    password_list += letters[random.randint(0,21)]
+
+for x in range(nr_numbers):
+    password_list += numbers[random.randint(0,9)]
+
+for x in range(nr_symbols):
+    password_list += symbols[random.randint(0,8)]
+
+random.shuffle(password_list)
+
+password = ''
+for char in password_list:
+    password += char
+
+print(f"Password is {password}")
