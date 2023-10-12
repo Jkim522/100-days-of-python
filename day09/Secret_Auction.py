f957d415271f105eb0800bf93cfbbc0ef0434843
Jkim522 <Jinseok.Kim522@gmail.com>
2023-10-12 18:14:10 -0400

    Update Secret_Auction.py

--- day09/Secret_Auction.py
+++ day09/Secret_Auction.py
@@ -1,1 +1,29 @@
+from art import logo
+import os
+print(logo)
 
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+
+dict = {} #Want {Jin: 102, John: 220}
+should_continue = True
+
+while should_continue:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? "))
+    dict[name] = bid
+    more_bidders = input("Are there other users who want to bid? yes or no ")
+    if more_bidders == "no":
+        should_continue = False
+    elif more_bidders == "yes":
+        clear_screen()
+
+highest_bid = 0
+winner = ""
+for name in dict:
+    if dict[name] > highest_bid:
+        highest_bid = dict[name]
+        winner = name
+
+print(f"Winning bid is {highest_bid} by {winner}")
